2a2782a913fd6bb20fb0a9ba4b86d429e6f3d9d3
RytAakarsh <aakarshmishra70@gmail.com>
2025-03-02 22:53:54 +0530

    files

--- FCFS/fcfs.js
+++ FCFS/fcfs.js
@@ -1,45 +1,3 @@
-// console.log("running...");
-
-// function fcfsDiskScheduling(requests, head) {
-//     let totalSeekTime = 0;
-//     let currentPosition = head;
-
-//     for (let request of requests) {
-//         totalSeekTime += Math.abs(request - currentPosition);
-//         currentPosition = request;
-//     }
-//     return totalSeekTime;
-// }
-
-// function scheduleprint ( numbers , head ){
-//     console.log(head);
-//     document.getElementById('sequencevalue').textContent = `${head}`;
-//     for ( let i=0 ; i<numbers.length ; i++ ){
-//         console.log(numbers[i]);
-//         document.getElementById('sequencevalue').textContent += `  , ${numbers[i]}`;
-//     }
-// }
-// const submitButton = document.querySelector('.submit');
-
-
-// submitButton.addEventListener('click', function(event) {
-//     event.preventDefault();
-
-//     const input1 = document.querySelector('.input1');
-//     const input2 = document.querySelector('.input2');
-
-//     const numbers1 = input1.value.split(',').map(Number);
-//     const numbers2 = Number(input2.value);
-
-//     console.log("Numbers from input1:", numbers1);
-//     console.log("Initial head position (numbers2):", numbers2);
-
-//     const seekValue = fcfsDiskScheduling(numbers1, numbers2);
-//     document.getElementById('seekvalue').textContent = `${seekValue}`;
-//     const ans = scheduleprint( numbers1  ,  numbers2)
-//     console.log("Total Seek Time:", seekValue);
-// });
-
 console.log("running...");
 
 function fcfsDiskScheduling(requests, head) {
@@ -61,10 +19,8 @@ function scheduleprint(numbers, head) {
         document.getElementById('sequencevalue').textContent += ` , ${numbers[i]}`;
     }
 }
-
-// Function to render the graph
 function renderGraph(numbers, head) {
-    let dataPoints = [{ y: head }]; // Start with head position
+    let dataPoints = [{ y: head }]; 
 
     numbers.forEach(num => {
         dataPoints.push({ y: num });
